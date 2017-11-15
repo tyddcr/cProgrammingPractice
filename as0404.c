551858f6b9b5af9a94d2cf59966f9e051b6a0280
tyddcr <32608579+tyddcr@users.noreply.github.com>
2017-11-15 20:48:15 +0800

    Update as0404.c

--- as0404.c
+++ as0404.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	const int PRECISION = 1000000;
 	double e = 1.0;
-	int temp = 1;
+	int temp = 1, n = 1;
 
-	while (temp <= PRECISION) {
-		e = e + 1 / (double) temp;
-		temp = temp * (temp + 1);
+	while (n <= PRECISION) {
+		e = e + 1 / (double) n;
+		temp++;
+		n *= temp;
 	}
 
 	printf("e = %f\n", e);
